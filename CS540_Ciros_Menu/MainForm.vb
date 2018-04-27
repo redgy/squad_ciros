d95c58bb8bbfe12cf6f5ee830d924f7091bcb69b
Redgy Canos <redgycanos@gmail.com>
2018-04-26 19:05:02 -0700

    Added MainForm within the visual studio

---
+++ CS540_Ciros_Menu/MainForm.vb
@@ -0,0 +1,10 @@
+﻿Public Class mainPage
+    Dim menuForm As MenuForm
+
+    Private Sub menuButton_Click(sender As Object, e As EventArgs) Handles menuButton.Click
+        menuForm = New MenuForm()
+        ' Me.Hide()
+        menuForm.Show()
+    End Sub
+
+End Class
--- CS540_Ciros_Menu/My Project/Application.Designer.vb
+++ CS540_Ciros_Menu/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.CS540_Ciros_Menu.Form1
+            Me.MainForm = Global.CS540_Ciros_Menu.mainPage
         End Sub
     End Class
 End Namespace
