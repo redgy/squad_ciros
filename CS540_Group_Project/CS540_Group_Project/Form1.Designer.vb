aaaf118a4e8a2ac90d93e10da8e89b1ed75915e4
Redgy Canos <redgycanos@gmail.com>
2018-04-26 22:01:01 -0700

    Started from scratch and template will be used for each individual form

---
+++ CS540_Group_Project/CS540_Group_Project/Form1.Designer.vb
@@ -0,0 +1,253 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cirosLogo = New System.Windows.Forms.PictureBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.saLabel = New System.Windows.Forms.Label()
+        Me.chineseLabel = New System.Windows.Forms.Label()
+        Me.frenchLabel = New System.Windows.Forms.Label()
+        Me.spanishLabel = New System.Windows.Forms.Label()
+        Me.englishLabel = New System.Windows.Forms.Label()
+        Me.dessertDrinkLabel = New System.Windows.Forms.Label()
+        Me.stromboliLabel = New System.Windows.Forms.Label()
+        Me.pizzaLabel = New System.Windows.Forms.Label()
+        Me.signatureSubLabel = New System.Windows.Forms.Label()
+        Me.saladLabel = New System.Windows.Forms.Label()
+        Me.appetizerLabel = New System.Windows.Forms.Label()
+        Me.background = New System.Windows.Forms.Label()
+        CType(Me.cirosLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(-6, -2)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(1012, 3)
+        Me.Label2.TabIndex = 31
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(-6, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(1012, 2)
+        Me.Label1.TabIndex = 30
+        '
+        'cirosLogo
+        '
+        Me.cirosLogo.BackColor = System.Drawing.Color.Firebrick
+        Me.cirosLogo.BackgroundImage = Global.CS540_Group_Project.My.Resources.Resources.CIROS2
+        Me.cirosLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.cirosLogo.Location = New System.Drawing.Point(9, 59)
+        Me.cirosLogo.Name = "cirosLogo"
+        Me.cirosLogo.Size = New System.Drawing.Size(315, 146)
+        Me.cirosLogo.TabIndex = 29
+        Me.cirosLogo.TabStop = False
+        '
+        'Label12
+        '
+        Me.Label12.BackColor = System.Drawing.Color.Firebrick
+        Me.Label12.Location = New System.Drawing.Point(583, -7)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(3, 68)
+        Me.Label12.TabIndex = 28
+        '
+        'saLabel
+        '
+        Me.saLabel.Font = New System.Drawing.Font("Lucida Sans Unicode", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.saLabel.ForeColor = System.Drawing.Color.Firebrick
+        Me.saLabel.Location = New System.Drawing.Point(942, 8)
+        Me.saLabel.Name = "saLabel"
+        Me.saLabel.Size = New System.Drawing.Size(53, 23)
+        Me.saLabel.TabIndex = 27
+        Me.saLabel.Text = "عربى"
+        Me.saLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'chineseLabel
+        '
+        Me.chineseLabel.Font = New System.Drawing.Font("Lucida Sans Unicode", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chineseLabel.ForeColor = System.Drawing.Color.Firebrick
+        Me.chineseLabel.Location = New System.Drawing.Point(883, 8)
+        Me.chineseLabel.Name = "chineseLabel"
+        Me.chineseLabel.Size = New System.Drawing.Size(53, 23)
+        Me.chineseLabel.TabIndex = 26
+        Me.chineseLabel.Text = "中文"
+        Me.chineseLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'frenchLabel
+        '
+        Me.frenchLabel.Font = New System.Drawing.Font("Lucida Sans Unicode", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.frenchLabel.ForeColor = System.Drawing.Color.Firebrick
+        Me.frenchLabel.Location = New System.Drawing.Point(784, 8)
+        Me.frenchLabel.Name = "frenchLabel"
+        Me.frenchLabel.Size = New System.Drawing.Size(93, 23)
+        Me.frenchLabel.TabIndex = 25
+        Me.frenchLabel.Text = "Français"
+        Me.frenchLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'spanishLabel
+        '
+        Me.spanishLabel.Font = New System.Drawing.Font("Lucida Sans Unicode", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.spanishLabel.ForeColor = System.Drawing.Color.Firebrick
+        Me.spanishLabel.Location = New System.Drawing.Point(683, 8)
+        Me.spanishLabel.Name = "spanishLabel"
+        Me.spanishLabel.Size = New System.Drawing.Size(95, 23)
+        Me.spanishLabel.TabIndex = 24
+        Me.spanishLabel.Text = "Español"
+        Me.spanishLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'englishLabel
+        '
+        Me.englishLabel.Font = New System.Drawing.Font("Lucida Sans Unicode", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.englishLabel.ForeColor = System.Drawing.Color.Firebrick
+        Me.englishLabel.Location = New System.Drawing.Point(592, 8)
+        Me.englishLabel.Name = "englishLabel"
+        Me.englishLabel.Size = New System.Drawing.Size(85, 23)
+        Me.englishLabel.TabIndex = 23
+        Me.englishLabel.Text = "English"
+        Me.englishLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'dessertDrinkLabel
+        '
+        Me.dessertDrinkLabel.Font = New System.Drawing.Font("Lucida Sans Unicode", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dessertDrinkLabel.ForeColor = System.Drawing.Color.Firebrick
+        Me.dessertDrinkLabel.Location = New System.Drawing.Point(443, 9)
+        Me.dessertDrinkLabel.Name = "dessertDrinkLabel"
+        Me.dessertDrinkLabel.Size = New System.Drawing.Size(133, 23)
+        Me.dessertDrinkLabel.TabIndex = 22
+        Me.dessertDrinkLabel.Text = "Desserts/Drinks"
+        Me.dessertDrinkLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'stromboliLabel
+        '
+        Me.stromboliLabel.Font = New System.Drawing.Font("Lucida Sans Unicode", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.stromboliLabel.ForeColor = System.Drawing.Color.Firebrick
+        Me.stromboliLabel.Location = New System.Drawing.Point(349, 9)
+        Me.stromboliLabel.Name = "stromboliLabel"
+        Me.stromboliLabel.Size = New System.Drawing.Size(96, 23)
+        Me.stromboliLabel.TabIndex = 21
+        Me.stromboliLabel.Text = "Stromboli"
+        Me.stromboliLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'pizzaLabel
+        '
+        Me.pizzaLabel.Font = New System.Drawing.Font("Lucida Sans Unicode", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.pizzaLabel.ForeColor = System.Drawing.Color.Firebrick
+        Me.pizzaLabel.Location = New System.Drawing.Point(296, 9)
+        Me.pizzaLabel.Name = "pizzaLabel"
+        Me.pizzaLabel.Size = New System.Drawing.Size(47, 23)
+        Me.pizzaLabel.TabIndex = 20
+        Me.pizzaLabel.Text = "Pizza"
+        Me.pizzaLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'signatureSubLabel
+        '
+        Me.signatureSubLabel.Font = New System.Drawing.Font("Lucida Sans Unicode", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.signatureSubLabel.ForeColor = System.Drawing.Color.Firebrick
+        Me.signatureSubLabel.Location = New System.Drawing.Point(167, 9)
+        Me.signatureSubLabel.Name = "signatureSubLabel"
+        Me.signatureSubLabel.Size = New System.Drawing.Size(122, 23)
+        Me.signatureSubLabel.TabIndex = 19
+        Me.signatureSubLabel.Text = "Signature Subs"
+        Me.signatureSubLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'saladLabel
+        '
+        Me.saladLabel.Font = New System.Drawing.Font("Lucida Sans Unicode", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.saladLabel.ForeColor = System.Drawing.Color.Firebrick
+        Me.saladLabel.Location = New System.Drawing.Point(105, 9)
+        Me.saladLabel.Name = "saladLabel"
+        Me.saladLabel.Size = New System.Drawing.Size(56, 23)
+        Me.saladLabel.TabIndex = 18
+        Me.saladLabel.Text = "Salads"
+        Me.saladLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'appetizerLabel
+        '
+        Me.appetizerLabel.Font = New System.Drawing.Font("Lucida Sans Unicode", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.appetizerLabel.ForeColor = System.Drawing.Color.Firebrick
+        Me.appetizerLabel.Location = New System.Drawing.Point(2, 9)
+        Me.appetizerLabel.Name = "appetizerLabel"
+        Me.appetizerLabel.Size = New System.Drawing.Size(97, 23)
+        Me.appetizerLabel.TabIndex = 17
+        Me.appetizerLabel.Text = "Appetizers"
+        Me.appetizerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'background
+        '
+        Me.background.BackColor = System.Drawing.Color.Firebrick
+        Me.background.Location = New System.Drawing.Point(-3, 44)
+        Me.background.Name = "background"
+        Me.background.Size = New System.Drawing.Size(1018, 691)
+        Me.background.TabIndex = 16
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1008, 729)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cirosLogo)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.saLabel)
+        Me.Controls.Add(Me.chineseLabel)
+        Me.Controls.Add(Me.frenchLabel)
+        Me.Controls.Add(Me.spanishLabel)
+        Me.Controls.Add(Me.englishLabel)
+        Me.Controls.Add(Me.dessertDrinkLabel)
+        Me.Controls.Add(Me.stromboliLabel)
+        Me.Controls.Add(Me.pizzaLabel)
+        Me.Controls.Add(Me.signatureSubLabel)
+        Me.Controls.Add(Me.saladLabel)
+        Me.Controls.Add(Me.appetizerLabel)
+        Me.Controls.Add(Me.background)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.cirosLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cirosLogo As PictureBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents saLabel As Label
+    Friend WithEvents chineseLabel As Label
+    Friend WithEvents frenchLabel As Label
+    Friend WithEvents spanishLabel As Label
+    Friend WithEvents englishLabel As Label
+    Friend WithEvents dessertDrinkLabel As Label
+    Friend WithEvents stromboliLabel As Label
+    Friend WithEvents pizzaLabel As Label
+    Friend WithEvents signatureSubLabel As Label
+    Friend WithEvents saladLabel As Label
+    Friend WithEvents appetizerLabel As Label
+    Friend WithEvents background As Label
+End Class
