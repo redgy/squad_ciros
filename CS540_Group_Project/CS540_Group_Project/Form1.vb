fd330704dc75cb13d3e7c3b5ad0c6c1a22d02312
Bonny Truong <bonny1357@yahoo.com>
2018-05-03 20:14:59 -0700

    fixed code to change forms

--- CS540_Group_Project/CS540_Group_Project/Form1.vb
+++ CS540_Group_Project/CS540_Group_Project/Form1.vb
@@ -21,31 +21,31 @@ Public Class Form1
         Me.Hide()
     End Sub
 
-    Private Sub saladLabel_Click(sender As Object, e As EventArgs)
+    Private Sub saladLabel_Click(sender As Object, e As EventArgs) Handles saladLabel.Click
         Dim f As New Salads
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub signatureSubLabel_Click(sender As Object, e As EventArgs)
+    Private Sub signatureSubLabel_Click(sender As Object, e As EventArgs) Handles signatureSubLabel.Click
         Dim f As New SignatureSubs
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub pizzaLabel_Click(sender As Object, e As EventArgs)
+    Private Sub pizzaLabel_Click(sender As Object, e As EventArgs) Handles pizzaLabel.Click
         Dim f As New Pizza
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub stromboliLabel_Click(sender As Object, e As EventArgs)
+    Private Sub stromboliLabel_Click(sender As Object, e As EventArgs) Handles stromboliLabel.Click
         Dim f As New Stromboli
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub dessertDrinkLabel_Click(sender As Object, e As EventArgs)
+    Private Sub dessertDrinkLabel_Click(sender As Object, e As EventArgs) Handles dessertDrinkLabel.Click
         Dim f As New DessertsDrinks
         f.Show()
         Me.Hide()
--- CS540_Group_Project/CS540_Group_Project/SignatureSubs.vb
+++ CS540_Group_Project/CS540_Group_Project/SignatureSubs.vb
@@ -1,34 +1,39 @@
 ﻿Public Class SignatureSubs
-    Private Sub appetizerLabel_Click(sender As Object, e As EventArgs)
+    Private Sub appetizerLabel_Click(sender As Object, e As EventArgs) Handles appetizerLabel.Click
         Dim f As New Appetizers
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub saladLabel_Click(sender As Object, e As EventArgs)
+    Private Sub saladLabel_Click(sender As Object, e As EventArgs) Handles saladLabel.Click
         Dim f As New Salads
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub pizzaLabel_Click(sender As Object, e As EventArgs)
+    Private Sub signatureSubLabel_Click(sender As Object, e As EventArgs) Handles signatureSubLabel.Click
+        Dim f As New SignatureSubs
+        f.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub pizzaLabel_Click(sender As Object, e As EventArgs) Handles pizzaLabel.Click
         Dim f As New Pizza
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub stromboliLabel_Click(sender As Object, e As EventArgs)
+    Private Sub stromboliLabel_Click(sender As Object, e As EventArgs) Handles stromboliLabel.Click
         Dim f As New Stromboli
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub dessertDrinkLabel_Click(sender As Object, e As EventArgs)
+    Private Sub dessertDrinkLabel_Click(sender As Object, e As EventArgs) Handles dessertDrinkLabel.Click
         Dim f As New DessertsDrinks
         f.Show()
         Me.Hide()
     End Sub
-
     Private Sub dinnersLabel_Click(sender As Object, e As EventArgs) Handles dinnersLabel.Click
         Dim f As New Dinners
         f.Show()
