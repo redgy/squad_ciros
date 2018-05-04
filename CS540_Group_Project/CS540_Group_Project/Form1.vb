ea520d9df7bb79f4e47e66ff5c9896459d0b05a3
Redgy Canos <redgycanos@gmail.com>
2018-05-03 19:30:40 -0700

    Initial backend sets current resx file to look at

--- CS540_Group_Project/CS540_Group_Project/Form1.vb
+++ CS540_Group_Project/CS540_Group_Project/Form1.vb
@@ -1,8 +1,45 @@
-﻿Public Class Form1
+﻿Imports System
+Imports System.Globalization
+Imports System.Resources
+Imports System.Threading
+
+Public Class Form1
+    ' ResX file variables
+    Dim resx_curr As ResXResourceSet
+    Dim resx_enUS As ResXResourceSet = New ResXResourceSet("en-US.resx")
+    Dim resx_esMX As ResXResourceSet = New ResXResourceSet("es-MX.resx")
+    Dim resx_frFR As ResXResourceSet = New ResXResourceSet("fr-FR.resx")
+    Dim resx_zhCHT As ResXResourceSet = New ResXResourceSet("zh-CHT.resx")
+    Dim resx_arSA As ResXResourceSet = New ResXResourceSet("ar-SA.resx")
+
+
     Private Sub appetizerLabel_Click(sender As Object, e As EventArgs) Handles appetizerLabel.Click
         Dim f As New Appetizers
         f.Show()
         Me.Hide()
     End Sub
 
+    Private Sub englishLabel_Click(sender As Object, e As EventArgs) Handles englishLabel.Click
+        resx_curr = resx_enUS
+    End Sub
+
+    Private Sub spanishLabel_Click(sender As Object, e As EventArgs) Handles spanishLabel.Click
+        resx_curr = resx_esMX
+    End Sub
+
+    Private Sub frenchLabel_Click(sender As Object, e As EventArgs) Handles frenchLabel.Click
+        resx_curr = resx_frFR
+    End Sub
+
+    Private Sub chineseLabel_Click(sender As Object, e As EventArgs) Handles chineseLabel.Click
+        resx_curr = resx_zhCHT
+    End Sub
+
+    Private Sub saLabel_Click(sender As Object, e As EventArgs) Handles saLabel.Click
+        resx_curr = resx_arSA
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        resx_curr = resx_enUS
+    End Sub
 End Class
