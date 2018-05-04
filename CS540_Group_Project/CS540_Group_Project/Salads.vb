2bc407b9f34672165a63e2eaddd68331b10729ef
Bonny Truong <bonny1357@yahoo.com>
2018-05-03 19:29:49 -0700

    changed formatting for salads form to add dinners tab

--- CS540_Group_Project/CS540_Group_Project/Salads.vb
+++ CS540_Group_Project/CS540_Group_Project/Salads.vb
@@ -1,23 +1,23 @@
 ﻿Public Class Salads
-    Private Sub signatureSubLabel_Click(sender As Object, e As EventArgs) Handles signatureSubLabel.Click
+    Private Sub signatureSubLabel_Click(sender As Object, e As EventArgs)
         Dim f As New SignatureSubs
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub pizzaLabel_Click(sender As Object, e As EventArgs) Handles pizzaLabel.Click
+    Private Sub pizzaLabel_Click(sender As Object, e As EventArgs)
         Dim f As New Pizza
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub stromboliLabel_Click(sender As Object, e As EventArgs) Handles stromboliLabel.Click
+    Private Sub stromboliLabel_Click(sender As Object, e As EventArgs)
         Dim f As New Stromboli
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub dessertDrinkLabel_Click(sender As Object, e As EventArgs) Handles dessertDrinkLabel.Click
+    Private Sub dessertDrinkLabel_Click(sender As Object, e As EventArgs)
         Dim f As New DessertsDrinks
         f.Show()
         Me.Hide()
