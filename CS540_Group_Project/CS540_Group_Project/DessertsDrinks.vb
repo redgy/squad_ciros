77e037701c920624c64260d403ee4e6778239a89
Maggie Lee <maglee732@gmail.com>
2018-05-03 19:27:21 -0700

    added fixed header to desserts/drinks page

--- CS540_Group_Project/CS540_Group_Project/DessertsDrinks.vb
+++ CS540_Group_Project/CS540_Group_Project/DessertsDrinks.vb
@@ -1,29 +1,29 @@
 ﻿Public Class DessertsDrinks
-    Private Sub saladLabel_Click(sender As Object, e As EventArgs) Handles saladLabel.Click
+    Private Sub saladLabel_Click(sender As Object, e As EventArgs)
         Dim f As New Salads
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub signatureSubLabel_Click(sender As Object, e As EventArgs) Handles signatureSubLabel.Click
+    Private Sub signatureSubLabel_Click(sender As Object, e As EventArgs)
         Dim f As New SignatureSubs
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub pizzaLabel_Click(sender As Object, e As EventArgs) Handles pizzaLabel.Click
+    Private Sub pizzaLabel_Click(sender As Object, e As EventArgs)
         Dim f As New Pizza
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub stromboliLabel_Click(sender As Object, e As EventArgs) Handles stromboliLabel.Click
+    Private Sub stromboliLabel_Click(sender As Object, e As EventArgs)
         Dim f As New Stromboli
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub appetizerLabel_Click(sender As Object, e As EventArgs) Handles appetizerLabel.Click
+    Private Sub appetizerLabel_Click(sender As Object, e As EventArgs)
         Dim f As New Appetizers
         f.Show()
         Me.Hide()
