3be70a78aa2f83db73c5d7ac66e2c4f73db325e8
Successful Student <COS-LabStudent@id.sdsu.edu>
2018-05-03 19:49:41 -0700

    added initial dinner design

--- CS540_Group_Project/CS540_Group_Project/Dinners.Designer.vb
+++ CS540_Group_Project/CS540_Group_Project/Dinners.Designer.vb
@@ -39,6 +39,21 @@ Partial Class Dinners
         Me.Label2 = New System.Windows.Forms.Label()
         Me.background = New System.Windows.Forms.Label()
         Me.cirosLogo = New System.Windows.Forms.PictureBox()
+        Me.dinner0 = New System.Windows.Forms.Label()
+        Me.dinner1 = New System.Windows.Forms.Label()
+        Me.dinner4 = New System.Windows.Forms.Label()
+        Me.dinner3 = New System.Windows.Forms.Label()
+        Me.dinner2 = New System.Windows.Forms.Label()
+        Me.dinner0d = New System.Windows.Forms.Label()
+        Me.dinner4d = New System.Windows.Forms.Label()
+        Me.dinner3d = New System.Windows.Forms.Label()
+        Me.dinner2d = New System.Windows.Forms.Label()
+        Me.dinner1d = New System.Windows.Forms.Label()
+        Me.dinner2price = New System.Windows.Forms.Label()
+        Me.dinner3price = New System.Windows.Forms.Label()
+        Me.dinner1price = New System.Windows.Forms.Label()
+        Me.dinner3add = New System.Windows.Forms.Label()
+        Me.dinner4price = New System.Windows.Forms.Label()
         CType(Me.cirosLogo, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -201,7 +216,7 @@ Partial Class Dinners
         'background
         '
         Me.background.BackColor = System.Drawing.Color.Firebrick
-        Me.background.Location = New System.Drawing.Point(-3, 44)
+        Me.background.Location = New System.Drawing.Point(-9, 42)
         Me.background.Name = "background"
         Me.background.Size = New System.Drawing.Size(1018, 691)
         Me.background.TabIndex = 45
@@ -217,12 +232,194 @@ Partial Class Dinners
         Me.cirosLogo.TabIndex = 60
         Me.cirosLogo.TabStop = False
         '
+        'dinner0
+        '
+        Me.dinner0.BackColor = System.Drawing.Color.Firebrick
+        Me.dinner0.Font = New System.Drawing.Font("Lucida Sans Unicode", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dinner0.Location = New System.Drawing.Point(335, 56)
+        Me.dinner0.Name = "dinner0"
+        Me.dinner0.Size = New System.Drawing.Size(307, 90)
+        Me.dinner0.TabIndex = 48
+        Me.dinner0.Text = "DINNERS"
+        '
+        'dinner1
+        '
+        Me.dinner1.BackColor = System.Drawing.Color.Firebrick
+        Me.dinner1.Font = New System.Drawing.Font("Lucida Sans Unicode", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dinner1.ForeColor = System.Drawing.Color.White
+        Me.dinner1.Location = New System.Drawing.Point(34, 195)
+        Me.dinner1.Name = "dinner1"
+        Me.dinner1.Size = New System.Drawing.Size(306, 46)
+        Me.dinner1.TabIndex = 62
+        Me.dinner1.Text = "Chicken Parmesan "
+        '
+        'dinner4
+        '
+        Me.dinner4.BackColor = System.Drawing.Color.Firebrick
+        Me.dinner4.Font = New System.Drawing.Font("Lucida Sans Unicode", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dinner4.ForeColor = System.Drawing.Color.White
+        Me.dinner4.Location = New System.Drawing.Point(551, 432)
+        Me.dinner4.Name = "dinner4"
+        Me.dinner4.Size = New System.Drawing.Size(308, 86)
+        Me.dinner4.TabIndex = 63
+        Me.dinner4.Text = "Baked Penne Pasta with Sausage"
+        '
+        'dinner3
+        '
+        Me.dinner3.BackColor = System.Drawing.Color.Firebrick
+        Me.dinner3.Font = New System.Drawing.Font("Lucida Sans Unicode", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dinner3.ForeColor = System.Drawing.Color.White
+        Me.dinner3.Location = New System.Drawing.Point(34, 447)
+        Me.dinner3.Name = "dinner3"
+        Me.dinner3.Size = New System.Drawing.Size(272, 46)
+        Me.dinner3.TabIndex = 64
+        Me.dinner3.Text = "Penne Pasta"
+        '
+        'dinner2
+        '
+        Me.dinner2.BackColor = System.Drawing.Color.Firebrick
+        Me.dinner2.Font = New System.Drawing.Font("Lucida Sans Unicode", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dinner2.ForeColor = System.Drawing.Color.White
+        Me.dinner2.Location = New System.Drawing.Point(573, 195)
+        Me.dinner2.Name = "dinner2"
+        Me.dinner2.Size = New System.Drawing.Size(272, 46)
+        Me.dinner2.TabIndex = 65
+        Me.dinner2.Text = "Meat Lasagna"
+        '
+        'dinner0d
+        '
+        Me.dinner0d.BackColor = System.Drawing.Color.Firebrick
+        Me.dinner0d.Font = New System.Drawing.Font("Lucida Sans Unicode", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dinner0d.ForeColor = System.Drawing.Color.White
+        Me.dinner0d.Location = New System.Drawing.Point(62, 66)
+        Me.dinner0d.Name = "dinner0d"
+        Me.dinner0d.Size = New System.Drawing.Size(267, 60)
+        Me.dinner0d.TabIndex = 66
+        Me.dinner0d.Text = "all dinners are served with 2 garlic knots"
+        '
+        'dinner4d
+        '
+        Me.dinner4d.BackColor = System.Drawing.Color.Firebrick
+        Me.dinner4d.Font = New System.Drawing.Font("Lucida Sans Unicode", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dinner4d.ForeColor = System.Drawing.Color.White
+        Me.dinner4d.Location = New System.Drawing.Point(562, 518)
+        Me.dinner4d.Name = "dinner4d"
+        Me.dinner4d.Size = New System.Drawing.Size(283, 96)
+        Me.dinner4d.TabIndex = 67
+        Me.dinner4d.Text = "homemade marinara sauce, fresh basil, ricotta, parmesan cheese topped with mozzar" &
+    "ella"
+        '
+        'dinner3d
+        '
+        Me.dinner3d.BackColor = System.Drawing.Color.Firebrick
+        Me.dinner3d.Font = New System.Drawing.Font("Lucida Sans Unicode", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dinner3d.ForeColor = System.Drawing.Color.White
+        Me.dinner3d.Location = New System.Drawing.Point(35, 518)
+        Me.dinner3d.Name = "dinner3d"
+        Me.dinner3d.Size = New System.Drawing.Size(294, 78)
+        Me.dinner3d.TabIndex = 68
+        Me.dinner3d.Text = "homemade marinara sauce, fresh basil, parmesan cheese"
+        '
+        'dinner2d
+        '
+        Me.dinner2d.BackColor = System.Drawing.Color.Firebrick
+        Me.dinner2d.Font = New System.Drawing.Font("Lucida Sans Unicode", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dinner2d.ForeColor = System.Drawing.Color.White
+        Me.dinner2d.Location = New System.Drawing.Point(574, 251)
+        Me.dinner2d.Name = "dinner2d"
+        Me.dinner2d.Size = New System.Drawing.Size(271, 132)
+        Me.dinner2d.TabIndex = 69
+        Me.dinner2d.Text = "prepared with our homemade meatball, sausage, ricotte, parmesan cheese, baked to " &
+    "perfection!"
+        '
+        'dinner1d
+        '
+        Me.dinner1d.BackColor = System.Drawing.Color.Firebrick
+        Me.dinner1d.Font = New System.Drawing.Font("Lucida Sans Unicode", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dinner1d.ForeColor = System.Drawing.Color.White
+        Me.dinner1d.Location = New System.Drawing.Point(35, 251)
+        Me.dinner1d.Name = "dinner1d"
+        Me.dinner1d.Size = New System.Drawing.Size(348, 132)
+        Me.dinner1d.TabIndex = 70
+        Me.dinner1d.Text = "topped with mozzarella cheese, served with penne pasta, tossed in our homemade ma" &
+    "rinara sauce, fresh basil, parmesan cheese"
+        '
+        'dinner2price
+        '
+        Me.dinner2price.BackColor = System.Drawing.Color.Firebrick
+        Me.dinner2price.Font = New System.Drawing.Font("Lucida Sans Unicode", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dinner2price.ForeColor = System.Drawing.Color.White
+        Me.dinner2price.Location = New System.Drawing.Point(804, 207)
+        Me.dinner2price.Name = "dinner2price"
+        Me.dinner2price.Size = New System.Drawing.Size(100, 23)
+        Me.dinner2price.TabIndex = 71
+        Me.dinner2price.Text = "$11.99"
+        '
+        'dinner3price
+        '
+        Me.dinner3price.BackColor = System.Drawing.Color.Firebrick
+        Me.dinner3price.Font = New System.Drawing.Font("Lucida Sans Unicode", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dinner3price.ForeColor = System.Drawing.Color.White
+        Me.dinner3price.Location = New System.Drawing.Point(240, 459)
+        Me.dinner3price.Name = "dinner3price"
+        Me.dinner3price.Size = New System.Drawing.Size(100, 23)
+        Me.dinner3price.TabIndex = 72
+        Me.dinner3price.Text = "$8.99"
+        '
+        'dinner1price
+        '
+        Me.dinner1price.BackColor = System.Drawing.Color.Firebrick
+        Me.dinner1price.Font = New System.Drawing.Font("Lucida Sans Unicode", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dinner1price.ForeColor = System.Drawing.Color.White
+        Me.dinner1price.Location = New System.Drawing.Point(344, 207)
+        Me.dinner1price.Name = "dinner1price"
+        Me.dinner1price.Size = New System.Drawing.Size(100, 23)
+        Me.dinner1price.TabIndex = 73
+        Me.dinner1price.Text = "$11.99"
+        '
+        'dinner3add
+        '
+        Me.dinner3add.BackColor = System.Drawing.Color.Firebrick
+        Me.dinner3add.Font = New System.Drawing.Font("Lucida Sans Unicode", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dinner3add.ForeColor = System.Drawing.Color.White
+        Me.dinner3add.Location = New System.Drawing.Point(36, 591)
+        Me.dinner3add.Name = "dinner3add"
+        Me.dinner3add.Size = New System.Drawing.Size(241, 23)
+        Me.dinner3add.TabIndex = 74
+        Me.dinner3add.Text = "add 2 meatballs for $3"
+        '
+        'dinner4price
+        '
+        Me.dinner4price.BackColor = System.Drawing.Color.Firebrick
+        Me.dinner4price.Font = New System.Drawing.Font("Lucida Sans Unicode", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dinner4price.ForeColor = System.Drawing.Color.White
+        Me.dinner4price.Location = New System.Drawing.Point(804, 480)
+        Me.dinner4price.Name = "dinner4price"
+        Me.dinner4price.Size = New System.Drawing.Size(100, 23)
+        Me.dinner4price.TabIndex = 75
+        Me.dinner4price.Text = "$11.99"
+        '
         'Dinners
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(1008, 729)
+        Me.Controls.Add(Me.dinner4price)
+        Me.Controls.Add(Me.dinner3add)
+        Me.Controls.Add(Me.dinner1price)
+        Me.Controls.Add(Me.dinner3price)
+        Me.Controls.Add(Me.dinner2price)
+        Me.Controls.Add(Me.dinner1d)
+        Me.Controls.Add(Me.dinner2d)
+        Me.Controls.Add(Me.dinner3d)
+        Me.Controls.Add(Me.dinner4d)
+        Me.Controls.Add(Me.dinner0d)
+        Me.Controls.Add(Me.dinner2)
+        Me.Controls.Add(Me.dinner3)
+        Me.Controls.Add(Me.dinner4)
+        Me.Controls.Add(Me.dinner1)
+        Me.Controls.Add(Me.dinner0)
         Me.Controls.Add(Me.cirosLogo)
         Me.Controls.Add(Me.background)
         Me.Controls.Add(Me.Label2)
@@ -264,4 +461,19 @@ Partial Class Dinners
     Friend WithEvents Label2 As Label
     Friend WithEvents background As Label
     Friend WithEvents cirosLogo As PictureBox
+    Friend WithEvents dinner0 As Label
+    Friend WithEvents dinner1 As Label
+    Friend WithEvents dinner4 As Label
+    Friend WithEvents dinner3 As Label
+    Friend WithEvents dinner2 As Label
+    Friend WithEvents dinner0d As Label
+    Friend WithEvents dinner4d As Label
+    Friend WithEvents dinner3d As Label
+    Friend WithEvents dinner2d As Label
+    Friend WithEvents dinner1d As Label
+    Friend WithEvents dinner2price As Label
+    Friend WithEvents dinner3price As Label
+    Friend WithEvents dinner1price As Label
+    Friend WithEvents dinner3add As Label
+    Friend WithEvents dinner4price As Label
 End Class
--- CS540_Group_Project/CS540_Group_Project/Dinners.vb
+++ CS540_Group_Project/CS540_Group_Project/Dinners.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Dinners
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles dinner0.Click
 
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles dinner1.Click
+
+    End Sub
 End Class
