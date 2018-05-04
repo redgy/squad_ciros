a22368b2cdaaae76b65bfc7b97884418e356316a
Maggie Lee <maglee732@gmail.com>
2018-05-03 19:13:24 -0700

    fixed header for stromboli page

--- CS540_Group_Project/CS540_Group_Project/Stromboli.vb
+++ CS540_Group_Project/CS540_Group_Project/Stromboli.vb
@@ -1,35 +1,75 @@
 ﻿Public Class Stromboli
-    Private Sub saladLabel_Click(sender As Object, e As EventArgs) Handles saladLabel.Click
+    Private Sub saladLabel_Click(sender As Object, e As EventArgs)
         Dim f As New Salads
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub signatureSubLabel_Click(sender As Object, e As EventArgs) Handles signatureSubLabel.Click
+    Private Sub signatureSubLabel_Click(sender As Object, e As EventArgs)
         Dim f As New SignatureSubs
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub pizzaLabel_Click(sender As Object, e As EventArgs) Handles pizzaLabel.Click
+    Private Sub pizzaLabel_Click(sender As Object, e As EventArgs)
         Dim f As New Pizza
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub dessertDrinkLabel_Click(sender As Object, e As EventArgs) Handles dessertDrinkLabel.Click
+    Private Sub dessertDrinkLabel_Click(sender As Object, e As EventArgs)
         Dim f As New DessertsDrinks
         f.Show()
         Me.Hide()
     End Sub
 
-    Private Sub appetizerLabel_Click(sender As Object, e As EventArgs) Handles appetizerLabel.Click
+    Private Sub appetizerLabel_Click(sender As Object, e As EventArgs)
         Dim f As New Appetizers
         f.Show()
         Me.Hide()
     End Sub
 
     Private Sub redPies1_Click(sender As Object, e As EventArgs) Handles redPies1.Click
+
+    End Sub
+
+    Private Sub stromboliLabel_Click(sender As Object, e As EventArgs) Handles MyBaseLabel.Click
+
+    End Sub
+
+    Private Sub Label12_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub englishLabel_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub spanishLabel_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub frenchLabel_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub chineseLabel_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub saLabel_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Stromboli_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
 
     End Sub
 End Class
